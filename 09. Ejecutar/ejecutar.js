cf80c31c3b0d03c952abeea3c21fdab40d45d2f5
D & D Sistemase <damiandiaz841@gmail.com>
2025-06-05 21:58:41 -0400

    Capitulo 09

---
+++ 09. Ejecutar/ejecutar.js
@@ -0,0 +1,11 @@
+const  exec = require('child_process').exec
+
+const x = 10;
+const y = 15;
+exec('script.sh' + x + ' ' + y, (err, stdout) => {
+    if (err) { 
+        throw err;
+    }
+    console.log('Comado ejecutado:');
+    console.log(stdout);
+});
---
+++ 09. Ejecutar/script.sh
@@ -0,0 +1,4 @@
+((suma = $1 + $2))
+
+echo "El Inico de un script"
+echo "La suma es: $suma"
