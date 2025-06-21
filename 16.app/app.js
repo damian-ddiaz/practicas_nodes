38289dc519faef5434a173a5247dc7dcfaa94b32
D & D sistemas <damiandiaz841@gmail.com>
2025-06-21 16:51:55 -0400

    Actualizacion icarosoft 21-06-2025 #2

--- 16.app/app.js
+++ 16.app/app.js
@@ -21,5 +21,31 @@ const pantalla = `
 
 mainScreen();
 function mainScreen(){
-    process.stdout.write('\033c'); // Limpiar pantalla
+    process.stdout.write('\033c'); 
+    interface.question(pantalla, res =>{
+        switch (res.trim()) {
+            case '1':
+                console.log('Elegiste la opcion 1: Crear nuevo documento');
+                createFile();
+                break;
+            case '2':
+                console.log('Elegiste la opcion 2: Abrir documento');
+                openFileInterface();
+                break;
+            case '3':
+                console.log('Elegiste la opcion 3: Cerrar editor');
+                interface.close();
+                break;
+            default:
+        }    
+
+    });// Limpiar pantalla
+} 
+
+function createFile() {
+
+}
+
+function openFileInterface() {
+
 }
